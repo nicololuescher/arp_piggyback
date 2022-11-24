207187dc8a06c3b251fe74d6c1b881f4e1e9aff0
Nicolo Lüscher <nicolo.luescher@gmail.com>
2022-11-24 21:14:50 +0100

    wip

--- arpSniffer/arpsniffer-bz2.py
+++ arpSniffer/arpsniffer-bz2.py
@@ -1,13 +1,8 @@
 # courtesy of https://stackoverflow.com/users/302831/santa and https://stackoverflow.com/users/3325230/user3325230
 import socket
 import bz2
-import hashlib
-import nacl.secret
-import nacl.utils
 
-rawSocket= socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
-
-src_mac = b'\x00\x0c\x29\x92\xe0\xcb'
+rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0806))
 
 while True:
 
@@ -16,9 +11,6 @@ while True:
     # candidate = packet[0][42:47]
     candidate = packet[0][42:]
     if(candidate[0:2] != b'\x00\x00'):
-        key = hashlib.sha256(src_mac).digest()
-        box = nacl.secret.SecretBox(key)
-        candidate = box.decrypt(candidate)
         candidate = bz2.decompress(candidate)
         if(candidate[0:5] == b'vula;'):
             descriptor = candidate[5:]
