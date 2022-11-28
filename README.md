7cfd6ac1d578d375cb9753ebb55546557cdb229c
Nicolo Lüscher <nicolo.luescher@gmail.com>
2022-11-28 22:59:00 +0100

    added timestamp

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # python_piggyback
-Use the ARP protocol to send messages across a censored network using python
+Use the ARP protocol to send encrypted messages in the trailer of an ARP packet.
+This encryption is used to smuggle a Vula descriptor to other Vula users running arp_piggyback on the same network segment.
+
+## Security
+The common sizes of ARP broadcast packets are between 42 (request) and 60
+(reply) bytes. The structure of the packet is such that a request and a reply
+usually have around 18 bytes of zero padding, if they are padded at all. It is
+possible to use those 18 bytes as a covert channel by filling them with random
+looking data.
+
+This encryption is not meant to forward-secret, it's simply to randomize the
+payload and defeat a trivial packet sniffers. The key is currently derived from
+a hash of the sender's MAC address. The payload is encrypted using an NaCL Box
+with a random nonce value. The length of the packet is much larger than the
+common 60 byte payload. Currently this means it is trivial to discover this
+protocol simply by counting packet lengths and performing trial decryption.
+
+The decryption process also decompresses the payload and it likely possible to
+exploit the decompression code.
+
+## Future work
+The sender will generate a compressed, encrypted payload and break it into n
+messages. The 18 byte padding which is normally zero will be the space where
+each chunk of the encrypted payload will be stored for sending.
+
+The receiver will sniff and build a payload in memory until the full payload is
+received for each MAC:IP pair. Each packet may trigger a trial decryption, and
+if it successfully decrypts, we know that we have discovered another
+arp_piggyback sender. We will then feed the descriptor into Vula.
+
+The sender and receiver will listen forever, sending and receiving peers as
+needed.
+
+## Get Vula descriptor and send it
+```
+vula verify my-descriptor| sudo python3 arpSender.py --interface wlp82s0 --descriptor -
+```
+
 
 ## remove arp peer and add it dynamically
 ```bash
--- arpSender/arpSender.py
+++ arpSender/arpSender.py
@@ -25,43 +25,58 @@ zero_mac = b"\x00\x00\x00\x00\x00\x00"
 @click.command()
 @click.option("--interface", type=str, help="interface to use for sending packets.")
 @click.option("--descriptor", type=str, help="descriptor to use for sending packets.")
-@click.option("--verbose", is_flag=True, default=False, help="show additional information.")
-@click.option("--is_reply", is_flag=True, default=False, help="Send reply packets instead of request.")
+@click.option(
+    "--verbose", is_flag=True, default=False, help="show additional information."
+)
+@click.option(
+    "--is_reply",
+    is_flag=True,
+    default=False,
+    help="Send reply packets instead of request.",
+)
 def main(interface, descriptor, verbose, is_reply):
     arp_cache = []
-
-    for device in os.popen('arp -a'):
+    for device in os.popen("arp -a"):
         _, ip, _, _, iface = device.split(maxsplit=4)
         if interface in iface:
-            arp_cache.append(ip[1:len(ip) -1])
-
+            arp_cache.append(ip[1 : len(ip) - 1])
 
     # open raw socket
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
     s.bind((interface, 0))
 
     # get dynamic information
-    src_mac = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s',  bytes(interface[:15], 'utf-8')))[18:24]
+    src_mac = fcntl.ioctl(
+        s.fileno(), 0x8927, struct.pack("256s", bytes(interface[:15], "utf-8"))
+    )[18:24]
     ip = socket.inet_aton(
         check_output(
             "ip -4 addr show " + interface + " | grep -oP '(?<=inet\s)\d+(\.\d+){3}'",
             shell=True,
         ).decode("utf-8")
     )
-    print(arp_cache)
-    
-    if len(arp_cache) == 0:
-        dest_ip = ip[0:3] + b'\x01'
-    else:
-        dest_ip = socket.inet_aton(random.choice(arp_cache))
 
-    if is_reply:
-        op_code = b'\x00\x02'
+    if len(arp_cache) == 0:
+        dest_ip = ip[0:3] + b"\x01"
     else:
-        op_code = b'\x00\x01'
+        random_ip = random.choice(arp_cache)
+        dest_ip = socket.inet_aton(random_ip)
+
+    if verbose:
+        print("Setting random arp ip as destination.")
+        print("Arp cache:", arp_cache)
+        print("Chosen Destination ip:", random_ip)
+
+    if verbose:
+        print("Sending packet as reply." if is_reply else "Sending packet as request.")
+    if is_reply:
+        op_code = b"\x00\x02"
+    else:
+        op_code = b"\x00\x01"
 
     # compress and encrypt descriptor
-    hd = bytes(descriptor, "utf-8")
+    message = str(int(time.time())) + descriptor
+    hd = bytes(message, "utf-8")
     hdc = zlib.compress(hd)
     key = hashlib.sha256(src_mac).digest()
     box = nacl.secret.SecretBox(key)
@@ -82,17 +97,33 @@ def main(interface, descriptor, verbose, is_reply):
     packet_header += protocol_size  # protocol size
     packet_header += op_code  # opcode
     packet_header += src_mac  # sender mac
-    packet_header += ip # sender ip
+    packet_header += ip  # sender ip
     packet_header += zero_mac  # destination mac
     packet_header += dest_ip  # destination ip
 
     # append message
     for i in range(math.ceil(len(encrypt) / (60 - len(packet_header) - 2))):
-        packet = packet_header + i.to_bytes(1, 'big') + b'\x64' + encrypt[i * ((60 - len(packet_header) - 2)): (i + 1) * ((60 - len(packet_header) - 2))]
-
+        packet = (
+            packet_header
+            + i.to_bytes(1, "big")
+            + b"\x64"
+            + encrypt[
+                i
+                * ((60 - len(packet_header) - 2)) : (i + 1)
+                * ((60 - len(packet_header) - 2))
+            ]
+        )
         # send packet
+        if verbose:
+            print(
+                "Sending packet",
+                i + 1,
+                "of",
+                math.ceil(len(encrypt) / (60 - len(packet_header) - 2)),
+            )
         time.sleep(random.uniform(0.01, 0.1))
         s.send(packet)
+
 
 if __name__ == "__main__":
     main()
