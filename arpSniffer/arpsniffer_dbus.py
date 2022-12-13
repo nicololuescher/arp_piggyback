20991fec4c0e75f27d02d849573cd990c0a69ff5
Yannick94 <yannick@ystebler.ch>
2022-12-13 19:06:56 +0100

    Fixed arpsniffer_dbus

--- arpSniffer/arpsniffer_dbus.py
+++ arpSniffer/arpsniffer_dbus.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 import pydbus
 
-import arpsniffer_constants
+import arpSender_constants
     
 def pydbusProcessDescriptorString(descriptorString):
     bus = pydbus.SystemBus()
-    _ORGANIZE_DBUS_NAME = arpsniffer_constants._ORGANIZE_DBUS_NAME
-    _ORGANIZE_DBUS_PATH = arpsniffer_constants._ORGANIZE_DBUS_PATH
+    _ORGANIZE_DBUS_NAME = arpSender_constants._ORGANIZE_DBUS_NAME
+    _ORGANIZE_DBUS_PATH = arpSender_constants._ORGANIZE_DBUS_PATH
     organize = bus.get(_ORGANIZE_DBUS_NAME, _ORGANIZE_DBUS_PATH)
     x = organize.process_descriptor_string(descriptorString)
     print(x)
 
 def pydbusGetLatestDescriptor():
     bus = pydbus.SystemBus()
-    organize = bus.get(arpsniffer_constants._DISCOVER_DBUS_NAME, arpsniffer_constants._ORGANIZE_DBUS_PATH)
+    _ORGANIZE_DBUS_NAME = arpSender_constants._ORGANIZE_DBUS_NAME
+    _ORGANIZE_DBUS_PATH = arpSender_constants._ORGANIZE_DBUS_PATH
+    organize = bus.get(_ORGANIZE_DBUS_NAME, _ORGANIZE_DBUS_PATH)
     x = organize.our_latest_descriptors()
-    print(x)
+    return x
     
